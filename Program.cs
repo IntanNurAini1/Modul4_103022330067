520ce30f6e794da7a59efa7c67ff6d8291541daf
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-11 06:52:15 +0700

    menambahkan class KodeProduk

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;   
+
+public class KodeProduk
+{
+    private static Dictionary<string, string> produk = new Dictionary<string, string>
+    {
+        {"Laptop", "E100"},
+        {"Smartphone", "E101"},
+        {"Tablet", "E102"},
+        {"Headset", "E103"},
+        {"Keyboard", "E104"},
+        {"Mouse", "E105"},
+        {"Printer", "E106"},
+        {"Monitor", "E107"},
+        {"Smartwatch", "E108"},
+        {"Kamera", "E109"}
+    };
+
+    public static string GetKodeProduk(string namaProduk)
+    {
+        if (produk.ContainsKey(namaProduk))
+        {
+            return produk[namaProduk];
+        }
+        else
+        {
+            return "Produk tidak ditemukan";
+
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Masukkan nama produk: ");
+        string namaProduk = Console.ReadLine();
+        string kodeProduk = KodeProduk.GetKodeProduk(namaProduk);
+        Console.WriteLine($"Kode untuk {namaProduk} adalah {kodeProduk}");
+    }
+}
